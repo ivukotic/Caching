666a9b256fb1d5b0992cf74d6cde00278a78f463
Ilija Vukotic <ivukotic@cern.ch>
2015-11-12 23:50:07 -0600

    dot

--- reporting/reportSummary.sh
+++ reporting/reportSummary.sh
@@ -2,4 +2,4 @@ cd /home/ivukotic/Caching/reporting
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 lsetup fax
-xrdcache_monitoring.py
+./xrdcache_monitoring.py
